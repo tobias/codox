7fbb0102c974abd656fe7a6daf166bb6eba3614d
Toby Crawley <toby@tcrawley.org>
2014-04-28 07:50:41 -0400

    Update version.

--- codox.core/project.clj
+++ codox.core/project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tcrawley/codox.core "0.6.3.2"
+(defproject org.clojars.tcrawley/codox.core "0.6.3.3"
   :description "Generate documentation from Clojure source files"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.namespace "0.2.4"]
